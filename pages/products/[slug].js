2b05d9f6cbb28ec4e23a427997bb492ad34b65f2
CJ Avilla <cjavilla@stripe.com>
2020-11-17 18:10:38 -0800

    Add readme.md and clean up console.log leftovers

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -8,23 +8,23 @@ const graphcms = new GraphQLClient(process.env.GRAPH_CMS_ENDPOINT);
 
 // getStaticPaths
 export async function getStaticPaths() {
-    const {products} = await graphcms.request(
-        gql`{
-          products {
-            name
-            slug
-          }
-        }
-      `
-    )
-    return {
-      paths: products.map(({slug}) => ({
-        params: {
-          slug
-        }
-      })),
-      fallback: false
-    };
+  const {products} = await graphcms.request(
+    gql`{
+      products {
+        name
+        slug
+      }
+    }
+    `
+  )
+  return {
+    paths: products.map(({slug}) => ({
+      params: {
+        slug
+      }
+    })),
+    fallback: false
+  };
 }
 
 // getStaticProps
@@ -32,72 +32,69 @@ export async function getStaticProps({params}) {
   const {product} = await graphcms.request(
     gql`
         query ProductPageQuery($slug: String!) {
-            product(where: {slug: $slug}) {
-              name
-              slug
-              price
-              images {
-                id
-                url
-                width
-                height
-              }
-            }
-        }
-      `,
-      {
-          slug: params.slug
+    product(where: {slug: $slug}) {
+      name
+      slug
+      price
+      images {
+        id
+        url
+        width
+        height
       }
+    }
+  }
+      `,
+    {
+      slug: params.slug
+    }
   )
   return  {
-      props: {
-          product
-      }
+    props: {
+      product
+    }
   }
 }
 
 const PayBtn = ({slug}) => {
-    const handleClick = async (e) => {
-        e.preventDefault();
-        console.log('before await');
-        const stripe = await stripePromise;
-        console.log(stripe);
-        console.log('after await');
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const stripe = await stripePromise;
 
-        // create checkout session
-        const session = await fetch('/api/create-checkout-session', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                slug: slug
-            })
-        }).then(resp => resp.json())
+    // create checkout session
+    const session = await fetch('/api/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        slug: slug
+      })
+    }).then(resp => resp.json())
 
-        console.log({session})
-        // redirect to checkout
-        const result = await stripe.redirectToCheckout({
-            sessionId: session.id
-        })
-    }
+    console.log({session})
+    // redirect to checkout
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id
+    })
+  }
 
-    return (
-        <button onClick={handleClick}>Buy!</button>
-    )
+  return (
+    <button onClick={handleClick}>Buy!</button>
+  )
 }
 
 const ProductPage = ({product}) => {
   const [image] = product.images
   return (
+    <div>
+      <>{product.name}</>
+      <>{product.price} EUR</>
       <div>
-        <>{product.name}</>
-        <>{product.price} EUR</>
-        <div>
-          <PayBtn slug={product.slug} />
-          <Image src={image.url} width={image.width} height={image.height} />
-        </div>
+        <PayBtn slug={product.slug} />
+        <Image src={image.url} width={image.width} height={image.height} />
       </div>
+    </div>
   )
 }
 
