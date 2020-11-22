c1e9003be482d9bf546d5fbdb7d1fd74dee26a92
Jonathan Steele <jonathan@steele.pro>
2020-11-22 15:45:38 +0000

    feat: Basic product grid on index

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/tailwind.css'
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="max-w-5xl mx-auto px-4">
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default App
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,77 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 import { graphCmsClient } from '../lib/graphCmsClient'
 
 export async function getStaticProps() {
-  // fetch products
   const { products } = await graphCmsClient.request(
     `
       {
         products {
-          name
-          slug
           id
+          images {
+            id
+            height
+            url
+            width
+          }
+          name
           price
+          slug
         }
       }
     `
   )
   return {
     props: {
-      products
+      products: products.map((product) => ({
+        ...product,
+        formattedPrice: new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'EUR',
+          minimumFractionDigits: 0
+        }).format(Number(product.price / 100))
+      }))
     }
   }
 }
 
 const IndexPage = ({ products }) => {
-  return products.map(({ name, slug, id, price }) => {
-    return (
-      <div>
-        <Link key={id} href={`/products/${slug}`}>
-          {name}
-        </Link>
+  return (
+    <div>
+      <div className="border-b py-4 space-y-2">
+        <h1 className="font-semibold text-3xl">Products</h1>
+        <p className="text-gray-500 text-sm">
+          Latest products available to buy!
+        </p>
       </div>
-    )
-  })
+      <div className="divide-y md:divide-y-0 md:divide-x grid md:grid-cols-3">
+        {products.map((product) => {
+          const [primaryImage] = product.images
+
+          return (
+            <div key={product.id} className="group">
+              <Link href={`/products/${product.slug}`}>
+                <a className="block px-8 py-6 space-y-4 group-hover:bg-gray-100">
+                  <Image
+                    src={primaryImage.url}
+                    height={primaryImage.height}
+                    width={primaryImage.width}
+                  />
+                  <div className="space-y-2">
+                    <h2 className="font-semibold text-2xl group-hover:text-blue-600">
+                      {product.name}
+                    </h2>
+                    <p className="text-gray-500">{product.formattedPrice}</p>
+                  </div>
+                </a>
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default IndexPage
