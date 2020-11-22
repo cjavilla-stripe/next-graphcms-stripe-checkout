51c28b9aca6046a6ba24d51231e61703b9dff1cf
Jonathan Steele <jonathan@steele.pro>
2020-11-22 16:10:50 +0000

    feat: Add navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
+import Link from 'next/link'
+
 import '../styles/tailwind.css'
 
 function App({ Component, pageProps }) {
   return (
     <div className="max-w-5xl mx-auto px-4">
+      <nav className="py-4">
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/">
+              <a className="font-semibold text-gray-500 text-sm hover:text-blue-600">
+                Home
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <Component {...pageProps} />
     </div>
   )
