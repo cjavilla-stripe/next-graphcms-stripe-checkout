f3c9b68b7985a4c000e886d16df1422ea362a316
Jonathan Steele <jonathan@steele.pro>
2020-11-22 16:11:04 +0000

    <PayBtn /> classes

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -96,7 +96,7 @@ const PayBtn = ({ formattedPrice, slug }) => {
     <button
       type="button"
       class={cc([
-        'items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 w-full md:w-auto',
+        'items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 w-full md:w-auto',
         {
           'cursor-not-allowed opacity-50': working
         }
